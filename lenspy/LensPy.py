b3b6deefed9b882bf84e50dfd97642a92f5db572
Toby <toby@tobias.codes>
2023-01-06 16:05:20 +0000

    update comment

--- lenspy/LensPy.py
+++ lenspy/LensPy.py
@@ -5,6 +5,8 @@ from web3.auto import w3
 from eth_account.messages import encode_defunct
 import random
 
+# these are random key pair from docs at 
+# https://web3js.readthedocs.io/en/v1.2.11/web3-eth-accounts.html
 pub = "0xb8CE9ab6943e0eCED004cDe8e3bBed6568B2Fa01"
 pri = "0x348ce564d427a3311b6536bbcff9390d69395b06ed6c486954e971d960fe8709"
 
