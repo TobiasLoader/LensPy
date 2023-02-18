17fde4679f1ac2c284f0bda71c1061dcbf894b11
Toby <toby@tobias.codes>
2023-02-18 02:08:06 +0000

    fixed signtypeddata in py!
    Broadcast from python works! used eth_utils to change typed data so encode_structured_data doesn't throw error

--- lenspy/helpers.py
+++ lenspy/helpers.py
@@ -1,57 +1,23 @@
-# as of Jan 2023,
-
-# Lens returns typed data responses for which the python equivalent of using _singTypedData (eth_account.messages.encode_structured_data and w3.eth.account.sign_message) doesn't work (*)
-# (*) being doing more research into this, and might be that it will work but currently still the signed message is not correct
-
-# The javascript _signTypedData is used by Lens
-# See the function 'getSig' in lens-protocol/core repo, in helpers/utils.ts file.
-
-# So here below is a beta python implementation of JS _signTypedData
-
-import ecdsa
+# import ecdsa
 from web3.auto import w3
-import eth_abi
-from eth_utils import decode_hex
+# import eth_abi
+import eth_utils
 
-# abiEncoded = eth_abi.encode_abi(['bytes', 'bytes32'], [input1, input2])
-# hash = w3.solidityKeccak(['bytes'], ['0x' + abiEncoded.hex()])
+## data types for eg. profileIds provided by Lens broadcast response
+## are not always compatible with 'eth_abi' conversions in 'encode_structured_data'
+## so in those cases:
+## use 'fix_abi_encode' to modify the input to 'encode_structured_data'
+## (this uses eth_utils.to_<type> function)
+## eg. Lens returns => profileId: '0x619d' with type 'uint256'
+##   this throws error because 'eth_abi' cannot convert '0x619d' to 'uint256'
+##   so 'fix_abi_encode' converts '0x619d' to '24989'
+##   then 'encode_structured_data' can use 'eth_abi' to convert '24989' to 'uint256'
 
-# also doesn't work atm
-def sign_typed_data(typed_data, private_key):
-	print(typed_data)
-	schema = []
-	names = []
-	types = []
-	values = []
-	for type_name,type_data in typed_data['types'].items():
-		for el in type_data:
-			schema.append(el['type'] + ' ' + el['name'])
-			names.append(el['name'])
-			types.append(el['type'])
-	i = 0
-	while i<len(names):
-		for value_name,value in typed_data['value'].items():
-			if value_name==names[i]:
-				# if value_name=='profileIds':
-					# decoded_hex = [decode_hex(v) for v in value]
-					# print(decoded_hex)
-					# encoded_uint256 = eth_abi.decode('uint256', decoded_hex)
-					# print(encoded_uint256)
-					# values.append(encoded_uint256)
-				# else:
-				values.append(value)
-				i += 1
-			if len(values)==len(names):
-				break
-	print(schema)
-	print(types)
-	print(values)
-	schema_hash = w3.solidityKeccak(["string" for i in range(len(schema))],schema)
-	values_hash = w3.solidityKeccak(types,values)
-	signature_hash = w3.solidityKeccak(["bytes32", "bytes32"],[schema_hash, values_hash])
-	signing_key = ecdsa.SigningKey.from_string(private_key, curve=ecdsa.SECP256k1)
-	return signing_key.sign(signature_hash)
-	
+def fix_abi_encode(els,type):
+	if type=='int':
+		return map(lambda v: eth_utils.to_int(hexstr=v), els)
+	elif type=='bytes':
+		return map(lambda v: eth_utils.to_bytes(hexstr=v), els)
 
 # prettify_api_query_str() parses and prettify's the graphql query strings stored in LensPy.api
 # it does this in 5 stages, 
@@ -126,3 +92,42 @@ def null_param(param):
 	if param==None:
 		return 'null'
 	return '"'+param+'"'
+
+
+
+## LEGACY
+
+# def sign_typed_data(typed_data, private_key):
+# 	print(typed_data)
+# 	schema = []
+# 	names = []
+# 	types = []
+# 	values = []
+# 	for type_name,type_data in typed_data['types'].items():
+# 		for el in type_data:
+# 			schema.append(el['type'] + ' ' + el['name'])
+# 			names.append(el['name'])
+# 			types.append(el['type'])
+# 	i = 0
+# 	while i<len(names):
+# 		for value_name,value in typed_data['value'].items():
+# 			if value_name==names[i]:
+# 				# if value_name=='profileIds':
+# 					# decoded_hex = [decode_hex(v) for v in value]
+# 					# print(decoded_hex)
+# 					# encoded_uint256 = eth_abi.decode('uint256', decoded_hex)
+# 					# print(encoded_uint256)
+# 					# values.append(encoded_uint256)
+# 				# else:
+# 				values.append(value)
+# 				i += 1
+# 			if len(values)==len(names):
+# 				break
+# 	print(schema)
+# 	print(types)
+# 	print(values)
+# 	schema_hash = w3.solidityKeccak(["string" for i in range(len(schema))],schema)
+# 	values_hash = w3.solidityKeccak(types,values)
+# 	signature_hash = w3.solidityKeccak(["bytes32", "bytes32"],[schema_hash, values_hash])
+# 	signing_key = ecdsa.SigningKey.from_string(private_key, curve=ecdsa.SECP256k1)
+# 	return signing_key.sign(signature_hash)
